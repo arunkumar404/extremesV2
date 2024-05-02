0b35fc5300b6893feb6198a46bdc8a4f00ba6ad3
Arun kumar <aravi@purecodesoftware.com>
2024-05-02 16:49:34 +0530

    tabs done

--- src/modules/Sidebar/SidebarContainer.js
+++ src/modules/Sidebar/SidebarContainer.js
@@ -3,22 +3,25 @@ import { Drawer, DrawerContent } from '../../components/ui/drawer'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Separator } from '../../components/ui/separator'
+import { Tabs, TabsList, TabsTrigger } from '../../components/ui/tabs'
 
 const SidebarContainer = ( { isDrawerOpen } ) => {
   const dispatch = useDispatch()
-  console.log("ffd",isDrawerOpen);
+  console.log( "ffd", isDrawerOpen );
 
   return (
     <Drawer direction={'left'} open={isDrawerOpen || false} onOpenChange={( value ) => dispatch( setOpenSidebarDrawer( value ) )}>
-      <DrawerContent className='removeAfter'>
-        <div className='flex flex-col h-full'>
-          <div className='flex justify-around px-3'>
-            <p className='font-bold text-lg'>MENS</p>
-            <Separator orientation="vertical" className='w-[2px]'/>
-            <p className='font-bold text-lg'>WOMENS</p>
-            <Separator orientation="vertical" className='w-[2px]' />
-            <p className='font-bold text-lg'>SALE</p>
-         </div>
+      <DrawerContent className='removeAfter bg-slate-100'>
+        <div className='items-center flex justify-center mt-[-20px] py-3 bg-white'>
+          <Tabs defaultValue="account" className="flex items-center justify-between bg-slate-200">
+            <TabsList className='bg-white'>
+              <TabsTrigger value="mens" className='bg-white'>MENS</TabsTrigger>
+              <Separator orientation="vertical" className='w-[2px] h-[20px]' />
+              <TabsTrigger value="womens text-lg" className='bg-white'>WOMENS</TabsTrigger>
+              <Separator orientation="vertical" className='w-[2px] h-[20px]' />
+              <TabsTrigger value="sale text-lg  " className='bg-white'>SALE</TabsTrigger>
+            </TabsList>
+          </Tabs>
         </div>
       </DrawerContent>
     </Drawer>
