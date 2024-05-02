4c8b02ada1d372bad5345b35943e7daf1931b337
Arun kumar <aravi@purecodesoftware.com>
2024-05-02 15:51:52 +0530

    sections added

--- src/modules/Sidebar/SidebarContainer.js
+++ src/modules/Sidebar/SidebarContainer.js
@@ -2,6 +2,7 @@ import { setOpenSidebarDrawer } from '../../store/main/slice'
 import { Drawer, DrawerContent } from '../../components/ui/drawer'
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { Separator } from '../../components/ui/separator'
 
 const SidebarContainer = ( { isDrawerOpen } ) => {
   const dispatch = useDispatch()
@@ -11,10 +12,12 @@ const SidebarContainer = ( { isDrawerOpen } ) => {
     <Drawer direction={'left'} open={isDrawerOpen || false} onOpenChange={( value ) => dispatch( setOpenSidebarDrawer( value ) )}>
       <DrawerContent className='removeAfter'>
         <div className='flex flex-col h-full'>
-          <div className='flex justify-around'>
-            <p>MEN</p>
-            <p>WOMEN</p>
-            <p>SALE</p>
+          <div className='flex justify-around px-3'>
+            <p className='font-bold text-lg'>MENS</p>
+            <Separator orientation="vertical" className='w-[2px]'/>
+            <p className='font-bold text-lg'>WOMENS</p>
+            <Separator orientation="vertical" className='w-[2px]' />
+            <p className='font-bold text-lg'>SALE</p>
          </div>
         </div>
       </DrawerContent>
